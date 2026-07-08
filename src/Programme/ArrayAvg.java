476098fdb145a813b930c56edf7bfc50ccf59879
Amit Yadav <ay3106222@gmail.com>
2026-07-08 20:01:13 +0530

    Array Average data find programme give error

--- src/Programme/ArrayAvg.java
+++ src/Programme/ArrayAvg.java
@@ -6,8 +6,13 @@ public class ArrayAvg {
         int[] arr_avg={25,36,48,69,45};
 
         int sum=0;
-        for (int i:arr_avg){
+        int arr_len=0;
+        for (int i = 1; i <arr_avg.length; i++) {
             sum+=i;
+
         }
+
+        int result=sum/arr_len;
+        System.out.println("Array List Average is:-"+result);
     }
 }
