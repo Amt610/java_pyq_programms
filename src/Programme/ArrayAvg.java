534d369dd3f9dc67e4211b28445171b0213bf122
Amit Yadav <ay3106222@gmail.com>
2026-07-08 18:56:06 +0530

    create average of array list first i'm calculate sum or array

---
+++ src/Programme/ArrayAvg.java
@@ -0,0 +1,13 @@
+package Programme;
+
+public class ArrayAvg {
+    public static void main(String[] args) {
+        //Array
+        int[] arr_avg={25,36,48,69,45};
+
+        int sum=0;
+        for (int i:arr_avg){
+            sum+=i;
+        }
+    }
+}
