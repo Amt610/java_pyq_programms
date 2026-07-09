dd4c13166cf724e31ade77f127c70e31f8c8f95c
Amit Yadav <ay3106222@gmail.com>
2026-07-09 15:00:06 +0530

    Reverse Array in java using array

---
+++ src/Array/ReverseArray.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        //Revere Array
+        int[] rev_arr={1,2,4,6,9,10};
+
+        for (int i = rev_arr.length; i >=0 ; i--) {
+            System.out.print(i+" ");
+        }
+    }
+}
